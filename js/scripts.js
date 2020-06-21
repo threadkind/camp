effc98c4293c36489cc1fa548048dc93c15b3e82
threadkind <threadkind@gmail.com>
2020-06-20 20:57:27 -0500

    Style media queries

--- js/scripts.js
+++ js/scripts.js
@@ -9,29 +9,53 @@ let page = {
 }
 
 let menu = {
-	1 : {
-		name : 'Home',
-		link : 'index.html'
+	links : {
+		1 : {
+			name : 'Home',
+			link : 'index.html'
+		},
+		2 : {
+			name : 'Merit Badges',
+			link : 'merit-badges.html'
+		},
+		3 : {
+			name : 'Bea TV Website',
+			link : 'https://bea-tv.com/'
+		},
 	},
-	2 : {
-		name : 'Merit Badges',
-		link : 'merit-badges.html'
-	},
-	3 : {
-		name : 'Bea TV Website',
-		link : 'https://bea-tv.com/'
-	},
-	4 : {
-		name : 'Social',
-		link : '#'
+	social : {
+		1 : {
+			name : 'Facebook',
+			image : `images/social/facebook.png`,
+			link : 'https://www.facebook.com/welcometobeatv'
+		},
+		2 : {
+			name : 'Instagram',
+			image : `images/social/instagram.png`,
+			link : 'https://www.instagram.com/welcometobeatv/'
+		},
+		3 : {
+			name : 'Twitter',
+			image : `images/social/twitter.png`,
+			link : 'https://twitter.com/welcometobeatv'
+		},
 	}
 }
 
 // ADD ITEMS TO MENU
 let nav = ''
-Object.keys(menu).forEach((key) => {
-	nav += `<li><a href="${menu[key].link}">${menu[key].name}</a></li>`
+
+Object.keys(menu.links).forEach((key) => {
+	nav += `<li><a href="${menu.links[key].link}">${menu.links[key].name}</a></li>`
 })
+
+nav += `<li id="socialLi">`
+
+Object.keys(menu.social).forEach((key) => {
+	nav += `<a href="${menu.social[key].link}" target="_blank"><div class="socialMenu"><img src="${menu.social[key].image}" alt="${menu.social[key].name} logo"></div></a>`
+
+})
+nav += `</li>`
 
 page.mainNav.innerHTML = nav;
 
